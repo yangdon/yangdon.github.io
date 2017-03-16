cad8723c256caf5e7d1a1d1d5a5e9d20d2ee183a
yangdon <1421689854@qq.com>
2017-03-16 22:04:11 +0800

    Site updated: 2017-03-16 22:04:11

---
+++ baidu_tongji.js
@@ -0,0 +1,12 @@
+<% if (theme.baidu_tongji) { %>
+<script type="text/javascript">
+#申请的百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?06ba7a0c685e58538745af909fea7a0b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<% } %>
